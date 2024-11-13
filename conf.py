d0479481fae5d43e1889d6cc63ef044744cfec4c
Jusong Yu <jusong.yeu@gmail.com>
2024-11-13 16:46:26 +0100

    Fix doc build

--- conf.py
+++ conf.py
@@ -3,7 +3,11 @@
 project = html_title = "AiiDA Enhancement Proposals"
 author = "The AiiDA team"
 
-extensions = ["myst_parser", "sphinx_external_toc"]
+extensions = [
+    "myst_parser", 
+    "sphinx_external_toc", 
+]
+
 exclude_patterns = [".github", ".tox", ".vscode", "_build"]
 external_toc_exclude_missing = True
 html_theme = "furo"
